ad725acd3c12dfd5c0f627b45b5c6a0b4e031133
John Doe <leehy0782@naver.com>
2022-02-16 07:35:55 +0900

    markup

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -7,7 +7,8 @@ function Main() {
         <>
         {/* <img src="img/me.png" alt="프로필 사진" /> */}
         <section className={`${style.main}`}>
-            <div className={`${style.picture}`}></div>
+            <div className={`${style.container}`}>
+                <div className={`${style.picture}`}></div>
                 <div className={`${style.textWrap}`}>
                     <div className={`${style.first}`}>
                         <p className={`${style.name}`}><b>Name</b> 이혜영</p>
@@ -19,13 +20,16 @@ function Main() {
                     <p className={`${style.police}`}><b>Degree</b> 울산대학교 경찰학과 졸업예정</p>
                     <p className={`${style.design}`}>울산대학교 디지털 콘텐츠 디자인학과 졸업예정</p>
                 </div>
+            </div>
                 {/* <div class={`${style.btnWrap}`}>
                     <button onclick="window.open('https://github.com/minbok-1998');" class="Github mybtn">Github</button>
                     <button onclick="window.open('https://velog.io/@minbok');" class="Velog mybtn">Velog</button>
                     <button class="mail mybtn">Email</button>
                     <button class="HP mybtn">HP</button>
                 </div> */}
+                <p className={`${style.growUp}`}>성실함과 끈기를 무기로 끊임없이 발전해 온, 더욱 발전해 나갈 이혜영입니다</p>
         </section>
+        
         </>
     )
 }
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,6 +3,20 @@ import style from '../css/Nav.module.css'
 function Nav() {
     return(
         <>
+        <div className={`${style.container}`}>
+            <div className={`${style.acc}`}>
+                <div className={`${style.hamburger}`}></div>
+                <div className={`${style.rowLine}`}></div>
+                <div className={`${style.dot}`}></div>
+                <p className={`${style.title}`}>Portfolio</p>
+            </div>
+            <ul className={`${style.nav}`}>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+            </ul>
+        
             <div className={`${style.contactWrap}`}>
                 <p className={`${style.contact}`}>Contact</p>
                 <div className={`${style.line}`}></div>
@@ -11,6 +25,7 @@ function Nav() {
                 <div>3</div>
                 <div>4</div>
             </div>
+        </div>
         </>
     )
 }
