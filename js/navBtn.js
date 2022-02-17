40060df4e7a5938dbe1cfd96c92e39588093f735
John Doe <leehy0782@naver.com>
2022-02-18 02:28:18 +0900

    create navBtn.js

---
+++ js/navBtn.js
@@ -0,0 +1,5 @@
+// navBtn
+const profileBtn = document.querySelector('.profileBtn');
+const strengthBtn = document.querySelector('.strengthBtn');
+const skillBtn = document.querySelector('.skillBtn');
+const projectBtn = document.querySelector('.projectBtn');
