66d62eb8d001c419d407d0cbf6231dbdeae672aa
John Doe <leehy0782@naver.com>
2022-02-18 02:34:53 +0900

    add animation on navBtn nonCheckedBtn

--- js/navBtn.js
+++ js/navBtn.js
@@ -1,5 +1,25 @@
 // navBtn
-const profileBtn = document.querySelector('.profileBtn');
 const strengthBtn = document.querySelector('.strengthBtn');
 const skillBtn = document.querySelector('.skillBtn');
 const projectBtn = document.querySelector('.projectBtn');
+
+strengthBtn.addEventListener('mouseover', function() {
+    strengthBtn.classList.add('expandBtn');
+})
+strengthBtn.addEventListener('mouseout', function() {
+    strengthBtn.classList.remove('expandBtn');
+})
+
+skillBtn.addEventListener('mouseover', function() {
+    skillBtn.classList.add('expandBtn');
+})
+skillBtn.addEventListener('mouseout', function() {
+    skillBtn.classList.remove('expandBtn');
+})
+
+projectBtn.addEventListener('mouseover', function() {
+    projectBtn.classList.add('expandBtn');
+})
+projectBtn.addEventListener('mouseout', function() {
+    projectBtn.classList.remove('expandBtn');
+})
