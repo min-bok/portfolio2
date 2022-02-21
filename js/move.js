63313ae577a56da6bd88aa05ce6f846260527cfd
John Doe <leehy0782@naver.com>
2022-02-22 04:41:30 +0900

    update project page

--- js/move.js
+++
@@ -1,9 +0,0 @@
-// const profile = document.querySelector('#profile');
-// const profiletoStrength = document.createElement('p');
-// profiletoStrength.innerText = 'Next'
-// profile.appendChild(profiletoStrength);
-// profiletoStrength.classList.add('profiletoStrength');
-
-// profiletoStrength.addEventListener('click', function() {
-
-// })
