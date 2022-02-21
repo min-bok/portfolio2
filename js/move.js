4eb70a8863dd64e64a12851b6174ebc7787614b4
John Doe <leehy0782@naver.com>
2022-02-22 04:00:46 +0900

    add mail

---
+++ js/move.js
@@ -0,0 +1,9 @@
+// const profile = document.querySelector('#profile');
+// const profiletoStrength = document.createElement('p');
+// profiletoStrength.innerText = 'Next'
+// profile.appendChild(profiletoStrength);
+// profiletoStrength.classList.add('profiletoStrength');
+
+// profiletoStrength.addEventListener('click', function() {
+
+// })
