62fe7285c86c0d601a966a9c4aacff4434f2b4dd
John Doe <leehy0782@naver.com>
2022-02-23 03:15:32 +0900

    update project markup

--- js/project.js
+++ js/project.js
@@ -1,17 +1,17 @@
-const hamburgerBtn = document.querySelector('.hamburgerBtn');
-const projectWrap = document.querySelector('#projectWrap');
-const closeBtn = document.querySelector('.closeBtn');
+// const hamburgerBtn = document.querySelector('.hamburgerBtn');
+// const projectWrap = document.querySelector('#projectWrap');
+// const closeBtn = document.querySelector('.closeBtn');
 
 
-projectWrap.classList.add('hidden');
+// projectWrap.classList.add('hidden');
 
-hamburgerBtn.addEventListener('click', function() {
-    projectWrap.classList.remove('hidden');
-    projectWrap.classList.add('flex');
-    projectWrap.classList.add('moveProject');
-});
+// hamburgerBtn.addEventListener('click', function() {
+//     projectWrap.classList.remove('hidden');
+//     projectWrap.classList.add('flex');
+//     projectWrap.classList.add('moveProject');
+// });
 
-closeBtn.addEventListener('click', function() {
-    projectWrap.classList.remove('flex');
-    projectWrap.classList.add('hidden');
-})
+// closeBtn.addEventListener('click', function() {
+//     projectWrap.classList.remove('flex');
+//     projectWrap.classList.add('hidden');
+// })
