088fa945973d4ad62c303469341757cfa17df11e
John Doe <leehy0782@naver.com>
2022-03-02 00:16:49 +0900

    add cookie

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,8 @@
+// Set a same-site cookie for first-party contexts
+document.cookie = 'cookie1=value1; SameSite=Lax';
+// Set a cross-site cookie for third-party contexts
+document.cookie = 'cookie2=value2; SameSite=None; Secure';
+
 // contact
 const githubText = document.querySelector('.githubText');
 const velogText = document.querySelector('.velogText');
@@ -50,4 +55,83 @@ phoneImg.addEventListener('mouseover', function() {
 })
 phoneImg.addEventListener('mouseout', function() {
     phoneText.classList.add('hidden');
+})
+
+const portfolioTitle = document.querySelector('.portfolioTitle');
+
+// profileBtnWrap
+const profileBtnWrap = document.querySelector('.profileBtnWrap');
+const profileBtn = document.querySelector('.profileBtn');
+const profileBtnText = document.querySelector('.profileBtnText');
+
+profileBtnText.classList.add('hidden');
+
+profileBtn.addEventListener('mouseover', function() {
+    profileBtnText.classList.remove('hidden');
+    profileBtn.classList.add('expandBtn');
+    
+})
+profileBtn.addEventListener('mouseout', function() {
+    profileBtnText.classList.add('hidden');
+    profileBtn.classList.remove('expandBtn');
+})
+profileBtn.addEventListener('click', function() {
+    portfolioTitle.innerText = 'Profile';
+})
+
+// strengthBtnWrap
+const strengthBtnWrap = document.queryCommandValue('.strengthBtnWrap');
+const strengthBtn = document.querySelector('.strengthBtn');
+const strengthBtnText = document.querySelector('.strengthBtnText');
+
+strengthBtnText.classList.add('hidden');
+
+strengthBtn.addEventListener('mouseover', function() {
+    strengthBtnText.classList.remove('hidden');
+    strengthBtn.classList.add('expandBtn');
+})
+strengthBtn.addEventListener('mouseout', function() {
+    strengthBtnText.classList.add('hidden');
+    strengthBtn.classList.remove('expandBtn');
+})
+strengthBtn.addEventListener('click', function() {
+    portfolioTitle.innerText = 'Strength';
+})
+
+// skillBtnWrap
+const skillBtnWrap = document.querySelector('.skillBtnWrap');
+const skillBtn = document.querySelector('.skillBtn');
+const skillBtnText = document.querySelector('.skillBtnText');
+
+skillBtnText.classList.add('hidden');
+
+skillBtn.addEventListener('mouseover', function() {
+    skillBtnText.classList.remove('hidden');
+    skillBtn.classList.add('expandBtn');
+})
+skillBtn.addEventListener('mouseout', function() {
+    skillBtnText.classList.add('hidden');
+    skillBtn.classList.remove('expandBtn');
+})
+skillBtn.addEventListener('click', function() {
+    portfolioTitle.innerText = 'Skill';
+})
+
+// projectBtnWrap
+const projectBtnWrap = document.querySelector('#project');
+const projectBtn = document.querySelector('.projectBtn');
+const projectBtnText= document.querySelector('.projectBtnText');
+
+projectBtnText.classList.add('hidden');
+
+projectBtn.addEventListener('mouseover', function() {
+    projectBtnText.classList.remove('hidden');
+    projectBtn.classList.add('expandBtn');
+})
+projectBtn.addEventListener('mouseout', function() {
+    projectBtnText.classList.add('hidden');
+    projectBtn.classList.remove('expandBtn');
+})
+projectBtn.addEventListener('click', function() {
+    portfolioTitle.innerText = 'Project';
 })
--- js/samesite.js
+++
@@ -1,3 +0,0 @@
-document.cookie = "cookie1=soo; SameSite=Lax"; 
-document.cookie = "cookie2=soo"; 
-document.cookie = "cookie3=hoo; SameSite=None; Secure";
