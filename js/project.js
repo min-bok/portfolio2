d8838089f610b8f9db5463da54348946710ac032
John Doe <leehy0782@naver.com>
2022-03-16 22:35:00 +0900

    update project

---
+++ js/project.js
@@ -0,0 +1,30 @@
+const projectOneDemo = document.querySelector('.projectOneDemo');
+const projectTwoDemo = document.querySelector('.projectTwoDemo');
+const projectThreeDemo = document.querySelector('.projectThreeDemo');
+const projectFourDemo = document.querySelector('.projectFourDemo');
+const projectFiveDemo = document.querySelector('.projectFiveDemo')
+const projectSixDemo = document.querySelector('.projectSixDemo')
+
+projectOneDemo.addEventListener('click', function() {
+    window.open('https://minbok-1998.github.io/Jump-Game/');
+})
+
+projectTwoDemo.addEventListener('click', function() {
+    window.open('https://minbok-1998.github.io/10000hours/');
+})
+
+projectThreeDemo.addEventListener('click', function() {
+    window.open('https://minbok-1998.github.io/Fan-letter/');
+})
+
+projectFourDemo.addEventListener('click', function() {
+    window.open('https://minbok-1998.github.io/portfolio2/');
+})
+
+projectFiveDemo.addEventListener('click', function() {
+    window.open('https://minbok-1998.github.io/HSKvoca/');
+})
+
+projectSixDemo.addEventListener('click', function() {
+    window.open('https://minbok-1998.github.io/landingpage/');
+})
