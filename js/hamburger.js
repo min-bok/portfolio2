97c70c4b56c3228231b79f07444e1dca5b520911
John Doe <leehy0782@naver.com>
2022-03-18 19:22:34 +0900

    update header home nav for mobile

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,9 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
 const hamburgerSpanOne = document.querySelector('.line:nth-child(1)');
 const hamburgerSpanThree = document.querySelector('.line:nth-child(3)');
-const show = document.querySelector('.show');
+const hide = document.querySelector('.hide');
 
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('is-active');
-    show.classList.toggle('hide');
+    hide.classList.toggle('show');
 })
