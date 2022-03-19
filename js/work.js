47da2c33636ffc7380a671a8ec8f09fffe9fe7b5
John Doe <leehy0782@naver.com>
2022-03-20 00:59:22 +0900

    update work.js

---
+++ js/work.js
@@ -0,0 +1,30 @@
+const workOne = document.querySelector('.workOne');
+const workTwo = document.querySelector('.workTwo');
+const workThree = document.querySelector('.workThree');
+const workFour = document.querySelector('.workFour');
+const workFive = document.querySelector('.workFive');
+const workSix = document.querySelector('.workSix');
+
+workOne.addEventListener('click', function() {
+    window.open('https://github.com/minbok-1998/Jump-Game');
+})
+
+workTwo.addEventListener('click', function() {
+    window.open('https://github.com/minbok-1998/10000hours');
+})
+
+workThree.addEventListener('click', function() {
+    window.open('https://github.com/minbok-1998/Fan-letter');
+})
+
+workFour.addEventListener('click', function() {
+    window.open('https://github.com/minbok-1998/portfolio2');
+})
+
+workFive.addEventListener('click', function() {
+    window.open('https://github.com/minbok-1998/MyVoca');
+})
+
+workSix.addEventListener('click', function() {
+    window.open('https://github.com/minbok-1998/landingpage');
+})
