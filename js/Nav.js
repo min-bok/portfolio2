11728e87d24796a7458adc4d53c15424807a99ea
John Doe <leehy0782@naver.com>
2022-03-19 23:16:27 +0900

    update nav.js

--- js/Nav.js
+++ js/Nav.js
@@ -1,3 +1,5 @@
+// hamburger btn
+
 const hamburger = document.querySelector('.hamburger');
 const hamburgerSpanOne = document.querySelector('.line:nth-child(1)');
 const hamburgerSpanThree = document.querySelector('.line:nth-child(3)');
@@ -6,4 +8,12 @@ const hide = document.querySelector('.hide');
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('is-active');
     hide.classList.toggle('show');
+    hamburgerSpanOne.classList.toggle('color');
+    hamburgerSpanThree.classList.toggle('color');
+    // setTimeout(function () {
+    //     hamburgerSpanOne.style.backgroundColor = 'green';
+    //     hamburgerSpanThree.style.backgroundColor = 'green';
+    // }, 1000)
 })
+
+// Mobile Nav
